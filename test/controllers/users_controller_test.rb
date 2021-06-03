76b8f55d2e822fe88eff935444fb0998f43751e7
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-06-03 22:49:50 +0900

    add: user sign up

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test 'should get new' do
+    # todo
+  end
+
+  test 'should get create' do
+    # todo
+  end
+end
