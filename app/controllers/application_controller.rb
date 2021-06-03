22b8961bbc73b96f235f9daf3ae0d50ed7516072
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-06-03 23:00:42 +0900

    add: login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def require_user_logged_in
+    redirect_to login_url unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
 end
