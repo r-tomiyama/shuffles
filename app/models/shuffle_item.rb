94200dc5985bf3bf64b2dc3c199a10bcdfa60bd1
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-07-31 21:20:20 +0900

    fix: 重複する選択肢を保存できないようにする

--- app/models/shuffle_item.rb
+++ app/models/shuffle_item.rb
@@ -2,5 +2,5 @@ class ShuffleItem < ApplicationRecord
   belongs_to :shuffle_list
 
   validates :name, presence: true
-  validates_uniqueness_of :name, uniquness: { scope: [:shuffle_list_id] } # TODO: 思ったように聞いてなさそう
+  validates_uniqueness_of :name, uniquness: { scope: [:shuffle_list_id] }
 end
--- app/models/shuffle_list.rb
+++ app/models/shuffle_list.rb
@@ -4,8 +4,15 @@ class ShuffleList < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validate :have_2_or_more_items
+  validate :forbid_same_items
 
   def have_2_or_more_items
     errors.add(:shuffle_items, :require_2_or_more) if shuffle_items.size < 2
+  end
+
+  def forbid_same_items
+    if (shuffle_items.length - shuffle_items.map(&:name).uniq.count) > 0
+      errors.add(:shuffle_items, :forbid_same_items)
+    end
   end
 end
