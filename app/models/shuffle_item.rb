25c7afaf861a5bed5969e098fc55272841fa85de
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-07-31 21:23:27 +0900

    fix: 別のシャッフル間で同一選択肢を保存できないバグを修正

--- app/models/shuffle_item.rb
+++ app/models/shuffle_item.rb
@@ -2,5 +2,5 @@ class ShuffleItem < ApplicationRecord
   belongs_to :shuffle_list
 
   validates :name, presence: true
-  validates_uniqueness_of :name, uniquness: { scope: [:shuffle_list_id] }
+  validates :name, uniqueness: { scope: :shuffle_list_id }
 end
