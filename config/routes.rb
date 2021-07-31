6b63321d16cb79cbba7030008ac980319561e60c
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-07-31 21:31:31 +0900

    fix: シャッフルをGETメソッドに変更

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post 'logout', to: 'sessions#destroy'
 
   resources :shuffle_lists, only: %i[index create edit update destroy]
-  post 'execute', to: 'shuffle_lists#execute'
+  get 'execute', to: 'shuffle_lists#execute'
 end
